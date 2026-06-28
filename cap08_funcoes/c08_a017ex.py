eb0cec7a60a72962a0637844806ccb5f129f1a0a
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-06-28 17:52:45 -0300

    Criando arquivo de lista de exercícios.

---
+++ cap08_funcoes/c08_a017ex.py
@@ -0,0 +1,16 @@
+'''
+8.9 – Mágicos: Crie uma lista de nomes de mágicos. Passe a lista para uma
+função chamada show_magicians() que exiba o nome de cada mágico da lista.
+
+8.10 – Grandes mágicos: Comece com uma cópia de seu programa do Exercício
+8.9. Escreva uma função chamada make_great() que modifique a lista de
+mágicos acrescentando a expressão o Grande ao nome de cada mágico. Chame
+show_magicians() para ver se a lista foi realmente modificada.
+
+8.11 – Mágicos inalterados: Comece com o trabalho feito no Exercício 8.10.
+Chame a função make_great() com uma cópia da lista de nomes de mágicos.
+Como a lista original não será alterada, devolva a nova lista e armazene-a em uma
+lista separada. Chame show_magicians() com cada lista para mostrar que você
+tem uma lista de nomes originais e uma lista com a expressão o Grande
+adicionada ao nome de cada mágico.
+'''
